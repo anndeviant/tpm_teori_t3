2856da349e7027af07c401487c159562a1d10cf1
Annas Sovianto <141797683+Reigentsu@users.noreply.github.com>
2025-03-28 16:22:02 +0700

    numtype

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tpm_teori_t3/login_page.dart';
+// import 'package:tpm_teori_t3/login_page.dart';
+import 'package:tpm_teori_t3/num_type.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: NumType(),
     );
   }
 }
