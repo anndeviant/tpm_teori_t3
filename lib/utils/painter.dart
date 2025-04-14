939fbf7cf7de207da0b6b7108db3e157ba9146e2
Lyan_Nandyan <lyannandyan@gmail.com>
2025-04-14 17:39:39 +0700

    mengganti ukuran painter

--- lib/utils/painter.dart
+++ lib/utils/painter.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HeadPainter extends CustomPainter {
@@ -11,46 +10,43 @@ class HeadPainter extends CustomPainter {
 
     paint.color = Color(0xffBA68C8);
     path = Path();
-    path.lineTo(0, size.height * 0.94);
+    path.lineTo(0, size.height * 1.12);
+    path.cubicTo(0, size.height, 0, size.height * 0.26, 0, size.height * 0.26);
+    path.cubicTo(0, size.height * 0.26, size.width, size.height * 0.26,
+        size.width, size.height * 0.26);
+    path.cubicTo(size.width, size.height * 0.26, size.width, size.height * 1.08,
+        size.width, size.height * 1.08);
+    path.cubicTo(size.width, size.height * 0.86, size.width * 0.93,
+        size.height * 0.74, size.width * 0.9, size.height * 0.71);
+    path.cubicTo(size.width * 0.86, size.height * 0.67, size.width * 0.84,
+        size.height * 0.67, size.width * 0.8, size.height * 0.71);
+    path.cubicTo(size.width * 0.75, size.height * 0.74, size.width * 0.6,
+        size.height * 0.98, size.width * 0.45, size.height * 1.16);
+    path.cubicTo(size.width * 0.31, size.height * 1.35, 0, size.height * 1.25,
+        0, size.height * 1.12);
     path.cubicTo(
-        0, size.height * 0.83, 0, size.height * 0.01, 0, size.height * 0.01);
-    path.cubicTo(0, size.height * 0.01, size.width / 2, size.height * 0.01,
-        size.width / 2, size.height * 0.01);
-    path.cubicTo(size.width / 2, size.height * 0.01, size.width,
-        size.height * 0.01, size.width, size.height * 0.01);
-    path.cubicTo(size.width, size.height * 0.01, size.width, size.height * 0.9,
-        size.width, size.height * 0.9);
-    path.cubicTo(size.width, size.height * 0.73, size.width * 0.92,
-        size.height * 0.49, size.width * 0.89, size.height * 0.46);
-    path.cubicTo(size.width * 0.86, size.height * 0.43, size.width * 0.85,
-        size.height * 0.43, size.width * 0.81, size.height * 0.46);
-    path.cubicTo(size.width * 0.77, size.height * 0.49, size.width * 0.57,
-        size.height * 0.8, size.width * 0.45, size.height * 0.92);
-    path.cubicTo(size.width * 0.34, size.height * 1.03, 0, size.height * 1.04,
-        0, size.height * 0.94);
-    path.cubicTo(
-        0, size.height * 0.94, 0, size.height * 0.94, 0, size.height * 0.94);
+        0, size.height * 1.12, 0, size.height * 1.12, 0, size.height * 1.12);
     canvas.drawPath(path, paint);
 
     // Path number 2
 
     paint.color = Color(0xff6A1B9A);
     path = Path();
-    path.lineTo(0, size.height * 0.68);
-    path.cubicTo(0, size.height * 0.58, 0, 0, 0, 0);
+    path.lineTo(0, size.height * 0.85);
+    path.cubicTo(0, size.height * 0.73, 0, 0, 0, 0);
     path.cubicTo(0, 0, size.width, 0, size.width, 0);
-    path.cubicTo(size.width, 0, size.width, size.height * 0.65, size.width,
-        size.height * 0.65);
-    path.cubicTo(size.width, size.height * 0.47, size.width * 0.92,
-        size.height * 0.24, size.width * 0.89, size.height / 5);
-    path.cubicTo(size.width * 0.86, size.height * 0.18, size.width * 0.85,
-        size.height * 0.18, size.width * 0.81, size.height / 5);
-    path.cubicTo(size.width * 0.77, size.height * 0.24, size.width * 0.57,
-        size.height * 0.55, size.width * 0.45, size.height * 0.66);
-    path.cubicTo(size.width * 0.34, size.height * 0.78, 0, size.height * 0.79,
-        0, size.height * 0.68);
+    path.cubicTo(size.width, 0, size.width, size.height * 0.81, size.width,
+        size.height * 0.81);
+    path.cubicTo(size.width, size.height * 0.59, size.width * 0.93,
+        size.height * 0.48, size.width * 0.9, size.height * 0.44);
+    path.cubicTo(size.width * 0.86, size.height * 0.41, size.width * 0.84,
+        size.height * 0.41, size.width * 0.8, size.height * 0.44);
+    path.cubicTo(size.width * 0.75, size.height * 0.48, size.width * 0.6,
+        size.height * 0.71, size.width * 0.45, size.height * 0.9);
+    path.cubicTo(size.width * 0.31, size.height * 1.08, 0, size.height * 0.98,
+        0, size.height * 0.85);
     path.cubicTo(
-        0, size.height * 0.68, 0, size.height * 0.68, 0, size.height * 0.68);
+        0, size.height * 0.85, 0, size.height * 0.85, 0, size.height * 0.85);
     canvas.drawPath(path, paint);
   }
 
